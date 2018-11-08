24d327cba3b7616ca397fbb93d58148adf1ab68d
Keith Harder <keithharder10@gmail.com>
2018-11-08 17:54:46 -0600

    pasted in my old code for reference

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oldish_used_code/Updatable.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Oldish_used_code;
+
+/**
+ * Created by user on 1/24/2018.
+ */
+
+public interface Updatable {
+    void update();
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Oldish_used_code/UpdatingManager.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Oldish_used_code;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by David on 1/24/2018.
+ */
+
+public class UpdatingManager extends Thread {
+
+    public volatile Set<Updatable> updatables = new LinkedHashSet<>();
+    public volatile LinearOpMode linearOpMode;
+    public volatile boolean killed;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void addUpdatable(Updatable updatable) {
+        updatables.add(updatable);
+    }
+
+    public void clearUpdatables(){
+        updatables.clear();
+    }
+
+    @Override
+    public void run() {
+        killed = false;
+        runtime.reset();
+        while (!(linearOpMode.isStopRequested() || killed) && !interrupted()) {
+            for (Updatable updatable : updatables) {
+                if(linearOpMode.isStopRequested() || killed)
+                    break;
+                if(updatable != null)
+                    updatable.update();
+            }
+            /*linearOpMode.telemetry.addData("Runtime", runtime.seconds());
+            linearOpMode.telemetry.update();*/
+        }
+    }
+
+    public void kill() {
+        killed = true;
+    }
+
+    public UpdatingManager(LinearOpMode linearOpMode) {
+        this.linearOpMode = linearOpMode;
+    }
+}
