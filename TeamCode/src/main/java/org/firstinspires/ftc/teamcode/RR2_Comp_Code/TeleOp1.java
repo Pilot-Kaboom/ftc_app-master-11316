b053eae629e867333d4f5349e65e129fc5416500
Keith Harder <keithharder10@gmail.com>
2018-11-30 16:40:02 -0600

    added dumping to gamepad1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/TeleOp1.java
@@ -10,7 +10,7 @@ public class TeleOp1 extends RR2_TeleBot {
         while(opModeIsActive()){
             //drive
             if(gamepad1.left_bumper){
-                drive.teledrive(-gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_trigger*.5, gamepad1.left_trigger*.5);
+                drive.teledrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_trigger*.5, gamepad1.left_trigger*.5);
             }
             else{
                 drive.teledrive(-gamepad1.left_stick_y, -gamepad1.left_stick_x, gamepad1.right_trigger*.5, gamepad1.left_trigger*.5);
@@ -28,7 +28,7 @@ public class TeleOp1 extends RR2_TeleBot {
             lifter.LiftOpss(gamepad2.dpad_up, gamepad2.dpad_down);
             //collecter
             collect.collect(-gamepad1.right_stick_y);
-            collect.drop(gamepad2.right_bumper, gamepad2.left_bumper);
+            collect.drop(gamepad2.right_bumper || gamepad1.right_bumper, gamepad2.left_bumper);
             //other stuff
             arm.Armtelem();
 
