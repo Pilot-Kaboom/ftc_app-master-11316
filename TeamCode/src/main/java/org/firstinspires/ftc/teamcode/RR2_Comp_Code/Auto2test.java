c1e744f4ddd8b87dd3edc80accce502a4d1b6ba3
Keith Harder <keithharder10@gmail.com>
2018-11-30 19:53:11 -0600

    revamped to be a landing auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/Auto2test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/Auto2test.java
@@ -2,91 +2,31 @@ package org.firstinspires.ftc.teamcode.RR2_Comp_Code;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="DON'T USE", group="Auto1")
+@Autonomous(name="just land", group="Auto1")
 public class Auto2test extends RR2_LiftBot {
 
     @Override
     public void run() {
         drive.resetEC();
-
-        while (opModeIsActive() && time.seconds() < 1.5){
-            lifter.LiftOps(.1);
+        //drop
+        while (opModeIsActive() && time.seconds() <.5){
+            lifter.LiftOps(-1);
+            doit=true;
+        }
+        while (opModeIsActive() && time.seconds() < 2){
+            lifter.LiftOps(1);
             //vert.setPower(1);
         }
+        lifter.LiftOps(0);
+        //vert.setPower(0);
         drive.resetEC();
         //hi ho robot, away!
-
-        drive.ECbackward(250, .25);
-        drive.StopMotors(0);
-        while(opModeIsActive() && sense.backD()<20){
-            drive.goRight(-.5);
-        }
-        while(opModeIsActive() && sense.backD()<24){
-            drive.goRight(-.25);
-        }
-        drive.StopMotors(0);
-        drive.resetEC();
-        //gold on far right
-        if(sense.colorL()> sense.white() && sense.colorR() > sense.white()){
-            drive.ECright(50, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECforward(500, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECleft(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECright(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-        }
-        //gold on right
-        else if(sense.colorL()>sense.white() && sense.colorR() < sense.white()){
-            drive.ECright(50, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECforward(250, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECleft(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECright(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-        }
-        //gold on left
-        else if(sense.colorL()<sense.white() && sense.colorR() < sense.white()){
-            drive.ECright(50, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECbackward(250, .5);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECleft(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECright(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-        }
-        //cant find gold
-        else {
-            drive.ECleft(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-            drive.ECright(100, .75);
-            drive.StopMotors(0);
-            drive.resetEC();
-            telemetry.addData("failed to find gold", 1);
-            telemetry.update();
-        }
-        while(opModeIsActive() && sense.sideD() > 5){
+        while(opModeIsActive() && drive.bect() <600 ){
             drive.goForward(-.5);
         }
-        while(opModeIsActive() ){
-
+        time.reset();
+        while(opModeIsActive() && time.seconds()< .25){
+            drive.StopMotors(0);
         }
 
     }
